975de712d479da680d88de3f534389419ee36d0e
mgruben <GrubenM@gmail.com>
2016-11-16 20:22:38 -0600

    Read input file into a list

--- CodedTriangleNumbers.py
+++ CodedTriangleNumbers.py
@@ -11,10 +11,19 @@ class Solution(object):
         
     def tri(self, n):
         return int(0.5 * n * (n + 1))
+
+def toInt(ch):
+    return ord(ch)
         
-def test():
+def Soltest():
     sol = Solution()
     print(sol.tri(10))
+    
+def readtest():
+    f = open('p042_words.txt')
+    for strings in f:
+        # strips leading and trailing ", then splits on ","
+        wordList = strings.strip("\"").split("\",\"")
 
 if __name__=="__main__":
-    test()
+    readtest()
